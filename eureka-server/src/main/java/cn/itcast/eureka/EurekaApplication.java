ef7409140c4ad24602b70325eee13a9a3b1824cf
fuck <fuck@123.com>
2023-08-31 20:54:25 +0800

    the second commit for eureka

---
+++ eureka-server/src/main/java/cn/itcast/eureka/EurekaApplication.java
@@ -0,0 +1,19 @@
+package cn.itcast.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+/**
+ * @Auther: 19640
+ * @Date: 2023/8/31 20:09
+ * @Description:
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
